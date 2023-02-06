40d563e4c1d400407f8177120381313eabb8651e
Sumayya Khalid <sumayyakhalid15.2k@gmail.com>
2023-02-06 21:15:46 +0500

    certificate details

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React from 'react'
 import "./Css/certificate_design.css"
 import { Header } from './Component/Header'
 import { Foooter } from './Component/Foooter'
-import { Certificate } from './Component/Certificate'
+import { Certificate_details } from './Component/Certificate_details'
+
 
 export const App = () => {
   return (
     <div>
      <Header/>
-     
-<Certificate/>
+   <Certificate_details/>
+
 <Foooter/>
 
     </div>
