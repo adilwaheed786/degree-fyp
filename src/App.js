1d23ebd2bbb7e2c3add89cbc35ff04e9730a8e6d
Sumayya Khalid <sumayyakhalid15.2k@gmail.com>
2023-02-13 22:23:16 +0500

    ts particles added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+// import Particle from './Component/Particle'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Header } from './Component/Header'
 import { Foooter } from './Component/Foooter'
@@ -9,10 +10,12 @@ import { AdminLogin } from './Component/Admin/Login/AdminLogin'
 import { Verify } from './Component/Admin/Authentication/Verify'
 import { StudentCertificate } from './Component/Admin/StudentDetail/StudentCertificate'
 import "./Css/certificate_design.css"
+import Particle from './Component/Particle'
 
 export const App = () => {
   return (
     <div>
+      <Particle/>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Header />}>
@@ -21,12 +24,18 @@ export const App = () => {
             <Route path="/verify-auth" element={<Verify />} />
             <Route path="/student-certificate" element={<StudentCertificate />} />
             <Route path="/confirm" element={<Certificate />} />
+            
             <Route path="/certificate_details" element={<Certificatedetails />} />
             <Route path="*" element={<NoPage />} />
+          
           </Route>
+           
         </Routes>
+        {/* <Particle/> */}
       </BrowserRouter>
+     
       <Foooter />
+     
     </div>
   )
 }
