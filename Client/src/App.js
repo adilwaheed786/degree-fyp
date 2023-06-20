dd089cfe4e109312a3295816b20dc744d911f5ee
Sumayya Khalid <sumayyakhalid15.2k@gmail.com>
2023-06-20 18:26:03 +0500

    Private root for admin

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,44 +1,43 @@
-import React from 'react'
-// import Particle from './Component/Particle'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { Header } from './Component/Header'
-import { Foooter } from './Component/Foooter'
-import { Certificate } from './Component/Certificate'
-import Homepage from './Component/Home_page/Homepage'
-import { Certificatedetails } from './Component/Certificatedetails'
-import { AdminLogin } from './Component/Admin/Login/AdminLogin'
-import { VerifyAuth } from './Component/Admin/Authentication/VerifyAuth'
-import { StudentCertificate } from './Component/Admin/StudentDetail/StudentCertificate'
-import { Verify } from './Component/VerificationPage/Verify'
-import "./Css/certificate_design.css"
-import Particle from './Component/Particle'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Header } from './Component/Header';
+import { Foooter } from './Component/Foooter';
+import { Certificate } from './Component/Certificate';
+import Homepage from './Component/Home_page/Homepage';
+import { Certificatedetails } from './Component/Certificatedetails';
+import { AdminLogin } from './Component/Admin/Login/AdminLogin';
+import { VerifyAuth } from './Component/Admin/Authentication/VerifyAuth';
+import { StudentCertificate } from './Component/Admin/StudentDetail/StudentCertificate';
+import { Verify } from './Component/VerificationPage/Verify';
+import './Css/certificate_design.css';
+import Particle from './Component/Particle';
 import CertificateList from './Component/Admin/Certificates/CertificateList';
+import { AuthProvider } from './Component/Admin/Authentication/AuthContext';
+import PrivateRoute from './Component/Admin/Authentication/PrivateRoute';
 
 export const App = () => {
   return (
     <div>
-      <Particle/>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Header />}>
-            <Route index element={<Homepage />} />
+      <Particle />
+      <AuthProvider>
+        <BrowserRouter>
+        <Header />
+          <Routes>
+          
             <Route path="/admin" element={<AdminLogin />} />
-            <Route path="/verify-auth" element={<VerifyAuth />} />
-            <Route path="/student-certificate" element={<StudentCertificate />} />
-            <Route path="/confirm" element={<Certificate />} />           
+            <Route path="/verify-auth" element={<PrivateRoute component={VerifyAuth}/>} />
+            <Route path="/student-certificate" element={<PrivateRoute component={StudentCertificate} />} />
+            <Route path="/confirm" element={<PrivateRoute component={Certificate} />} />
             <Route path="/certificate_details" element={<Certificatedetails />} />
             <Route path="/verify" element={<Verify />} />
-            <Route path="/CertificatesList" element={<CertificateList />} />
+            <Route path="/CertificatesList" element={<PrivateRoute component={CertificateList} />} />
             <Route path="*" element={<Homepage />} />
-          </Route>
-           
-        </Routes>
-        {/* <Particle/> */}
-      </BrowserRouter>
-     
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
       <Foooter />
-     
     </div>
-  )
-}
-export default App
+  );
+};
+
+export default App;
---
+++ Client/src/Component/Admin/Authentication/AuthContext.js
@@ -0,0 +1,23 @@
+import React, { createContext, useState } from 'react';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = () => {
+    // Perform login logic, such as verifying credentials or checking authentication token
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    // Perform logout logic, such as clearing authentication token
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
