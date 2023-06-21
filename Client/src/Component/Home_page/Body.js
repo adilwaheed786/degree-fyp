dbd2e247afc9b05176b8953bf2c0fee6073c0a61
Adil Waheed <adilwaheed2222.com@gmail.com>
2023-06-22 03:47:25 +0500

    Document Hashed And Email Sent SuccessFully

--- Client/src/Component/Home_page/Body.js
+++ Client/src/Component/Home_page/Body.js
@@ -6,12 +6,17 @@ import { useCallback, useState, useRef } from "react";
 import { Navigate, useHistory, useNavigate }  from 'react-router-dom';
 import axios from "axios";
 import { useDropzone } from "react-dropzone";
+import Web3 from 'web3';
+import StudentCertificateContract from '../../build/contracts/StudentCertificateContract.json';
 
 const Body = () => {
   const [pdfFile, setPdfFile] = useState(null);
   const [hash, setHash] = useState("");
   const inputRef = useRef(null);
-
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [valid, setValid] = useState(false);
+  const [notvalid, setNotValid] = useState(null);
   const handleDrop = useCallback((acceptedFiles) => {
     setPdfFile(acceptedFiles[0]);
   }, []);
@@ -24,13 +29,17 @@ const Body = () => {
 
   const handleClick = () => {
     inputRef.current.click();
+    setNotValid(false);
+    setValid(false);
   };
 
   const navigate =useNavigate()
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-
+    setLoading(true);
+    setNotValid(false);
+    setValid(false);
     if (!pdfFile) {
       console.error("No PDF file selected");
       return;
@@ -40,26 +49,64 @@ const Body = () => {
     formData.append("pdfFile", pdfFile);
 
     try {
+      setError(null)
       // Send the POST request to the server
       const response = await axios.post("/api/upload", formData, {
         headers: {
           "Content-Type": "multipart/form-data",
         },
       });
-
       // Handle the response from the server
-      const { hash } = response.data;
-       // Extract the hash value
-      console.log("Hash:", hash); // Print hash value in the console
-      setHash(hash);
-    
+      if (response.status === 200) {
+       const documenthash=`${response.data.hash}`;
+       console.log("Hash:", documenthash); // Print hash value in the console
+       setHash(documenthash);
+       try {
+        //const web3 = new Web3(window.ethereum);
+       // Connect to the local Ethereum network provided by Ganache
+        const ganacheUrl = 'http://localhost:7545';
+        const web3 = new Web3(ganacheUrl);
+        const networkId = await web3.eth.net.getId();
+        const deployedNetwork = StudentCertificateContract.networks[networkId];
+        const contractAddress = deployedNetwork.address;
+  
+        const contract = new web3.eth.Contract(
+          StudentCertificateContract.abi,
+          contractAddress
+        );
+        setLoading(true); // Show the loading screen
+        // Assuming you have already connected to the Ethereum network and instantiated the contract object
+        const Verified = await contract.methods.verifyDocumentHash(hash).call();
+       // const totalCertificates = await contract.methods.getTotalCertificatesCount().call();
+        if(Verified){
+          setValid(true)
+        }
+        else{
+          setNotValid(true)
+        }
+       console.log(Verified);
+        setLoading(false); // Hide the loading screen
+      } catch (error) {
+        setLoading(false);
+        console.log('Error fetching certificates:', error);
+        setError('An error occurred. Please try again later.');
+      }
+       setLoading(false);
+      }
+      else {
+
+        console.error("Upload failed:", response.data);
+        setError('Upload Failed')
+      }
+      
 
     } catch (error) {
       // Handle any errors
       console.error("Error uploading PDF file:", error);
+      setError('Error uploading PDF file:');
     }
   };
-
+ 
   return (
     <div className="row body-row">
       <div className="col-md-2 column1">
@@ -105,6 +152,26 @@ const Body = () => {
                 Upload Degree File
               </button>
             </div>
+            {loading && (     
+      <div className="alert alert-primary m-2" role="alert">
+      <div >Loading Please Wait...</div>
+      </div>
+      )}
+      {error && (       
+        <div className="alert alert-danger m-2" role="alert">
+          {error}
+        </div>
+      )}
+      {valid && (       
+        <div className="alert alert-success m-2" role="alert">
+        <div >Degree  Is Verified From BlockChain</div>
+        </div>
+      )}
+      {notvalid && (       
+        <div className="alert alert-danger m-2" role="alert">
+        <div >OOP! Degree  Is Not Verified From BlockChain</div>
+        </div>
+      )}
           </form>
         </div>
       </div>
