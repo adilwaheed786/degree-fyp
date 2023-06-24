4c968a06ebee90c2aceee2b070b47c9c54ce6423
Rabyaessani <rabbiaessani52@gmail.com>
2023-06-24 15:05:38 +0500

    Copyrightin Footer

--- Client/src/Component/Foooter.js
+++ Client/src/Component/Foooter.js
@@ -4,10 +4,12 @@ export const Foooter = () => {
     <div className='footer-main'>
         {/* ------Footer code start */}
 <div className="footer">
-
-<p>DEGREE VERIFICATION SYSTEM 
+<p style={{display:'inline'}}>
+<a href="https://www.bahria.edu.pk/"style={{display:'inline',color:'white'}} className='Bahrialink'>Bahria University</a>
 <i class="fa fa-copyright" style={{margin:"10px",fontWeight:"bolder"}}></i>
 </p>
+<p style={{display:'inline'}} className='title'>Degree Verifiction System ---</p>
+<p style={{display:'inline'}} className='Names'> FYP by Adil Waheed, Rabya Essani & Sumayya Khalid</p>
 
 </div>
 {/* ------Footer code end */}
